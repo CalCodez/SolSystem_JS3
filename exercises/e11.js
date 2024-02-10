2672edcfbb3edd74dc7586fc31bb076a22311194
Calvin Cortez Colbert <CalCodez@outlook.com>
2024-02-09 21:03:09 -0600

    ex11 passed

--- exercises/e11.js
+++ exercises/e11.js
@@ -6,7 +6,12 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function lowMoonsPlanets(data) {
-  // Your code goes here...
+  const lessTenMoons = [];
+  data.planets.map(function (planets) {
+    if (planets.moonsCount < 10 || planets.moonsCount == null)
+      lessTenMoons.push(planets.name)
+  })
+  return lessTenMoons;
 }
 
 // === TEST YOURSELF ===
