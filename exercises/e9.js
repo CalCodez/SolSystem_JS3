b5b789a4b2a76f8047b9938ee6189134d8f53087
Calvin Cortez Colbert <CalCodez@outlook.com>
2024-02-09 20:15:59 -0600

    ex9 passed ex8 needs refractored

--- exercises/e9.js
+++ exercises/e9.js
@@ -5,7 +5,10 @@ import { data } from "../data/data";
 // Return example: { key1: value1, key2: value2, ... , keyN: valueN }
 
 export function getEarthData(data) {
-  // Your code goes here...
+  const planetEarth = data.planets.find(function (planets) {
+    return planets.id == 'earth';
+  })
+  return planetEarth;
 }
 
 
